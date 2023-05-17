0a075f11739379c910c75c4eeddd96c08f9f8f2b
Erik <eric.lasic12345@gmail.com>
2023-05-17 18:10:22 +0200

    -Dodan gumb za izhod iz OneRecipeActivity -Odstranjena opcija za izbris na LongClick -Popravljen izgled receptov (brez complexity in type)

--- app/src/main/java/com/example/cookitup/RecipeAdapter.kt
+++ app/src/main/java/com/example/cookitup/RecipeAdapter.kt
@@ -15,15 +15,13 @@ class RecipeAdapter(var recipes:MutableList<Recipe>) : RecyclerView.Adapter<Reci
 
     interface MyOnClick {
         fun onClick(p0: View?, position:Int)
-        fun onLongClick(p0: View?, position:Int)
+        //fun onLongClick(p0: View?, position:Int)
     }
 
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val imageView: ImageView = itemView.findViewById(R.id.imageView9)
         val name: TextView = itemView.findViewById(R.id.recipe_name)
-        val type: TextView = itemView.findViewById(R.id.recipe_type)
         val prep: TextView = itemView.findViewById(R.id.recipe_prep)
-        val diff: TextView = itemView.findViewById(R.id.recipe_diff)
         val line: CardView = itemView.findViewById(R.id.cvLine)
     }
 
@@ -39,17 +37,17 @@ class RecipeAdapter(var recipes:MutableList<Recipe>) : RecyclerView.Adapter<Reci
         Picasso.get().load("https://icons.iconarchive.com/icons/iconarchive/fat-sugar-food/256/Pizza-icon.png").into(holder.imageView)
 
         holder.name.text = recipes.name
-        holder.type.text = recipes.type
         holder.prep.text = recipes.time.toString() + " min"
-        holder.diff.text = recipes.complexity
 
         holder.line.setOnClickListener { p0 ->
             onClickObject.onClick(p0, holder.bindingAdapterPosition) //Action from Activity
         }
+        /*
         holder.line.setOnLongClickListener { p0 ->
             onClickObject.onLongClick(p0, holder.bindingAdapterPosition)
             true
         }
+         */
     }
     override fun getItemCount(): Int {
         return recipes.size
